50b936c303f6ea5ebaed1b3bb1942b693d871250
aimz <aimzbooking@gmail.com>
2017-09-04 13:31:10 +0200

    bootstraaap

--- StudentCatalog/Controllers/StudentController.cs
+++ StudentCatalog/Controllers/StudentController.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using StudentCatalog.Models.Entites;
 
 namespace test.Controllers
 {
     public class StudentController : Controller
     {
+        List<Student> students = new List<Student>();
+
         public IActionResult Index()
         {
+            students.Add(new Student{FirstName="Asam", LastName="Ali", Age=23});
+            students.Add(new Student{FirstName="Masa", LastName="Lia", Age=42});
+            students.Add(new Student{FirstName="Sama", LastName="Ila", Age=11});
+
+            ViewBag.stud = students;
+
             return View();
         }
 
@@ -19,7 +28,5 @@ namespace test.Controllers
             ViewBag.number = id;
             return View();
         }
-
-        
     }
 }
